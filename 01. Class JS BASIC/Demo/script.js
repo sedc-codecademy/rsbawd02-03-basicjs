e3b9374831184ca5d4277bf428e2e1fd7b7b7d21
Jelena <jelena-miodragovic@live.com>
2024-01-16 22:51:31 +0100

    01. Class JS BASIC

---
+++ 01. Class JS BASIC/Demo/script.js
@@ -0,0 +1,126 @@
+// function sum(a, b) {
+//     console.log(a + b);
+//  }
+// var c, d; //deklaracija varijabli c i d
+// sum(c, d);//NaN
+
+// console.log(c, d);//undefined, undefined
+
+// var e = 2;
+// var f = 6; //definicija varijabli e i f
+
+// var a = b  = c = 5;
+
+// sum(c, b);//10
+// sum(c = 2, b = 7);//10
+
+//var TestVar; // This variable is not defined, that's why it's undefined
+//alert(TestVar); //shows undefined
+//alert(typeof TestVar); //shows undefined
+
+// var TestVar = null;
+// alert(TestVar); //shows null
+// alert(typeof TestVar); //shows object
+
+// var broj = 5;
+// alert(broj); //shows 5
+// alert(typeof broj); //shows number
+
+// var broj = "5";
+// alert(broj); //shows 5
+// alert(typeof broj); //shows number
+
+// var string = "Jelena";
+// alert(string); //shows Jelena
+// alert(typeof string); //shows string
+
+// This is a string
+// var firstName = "Marko";
+// var lastName = "Markovic";
+// console.log(firstName, "firstName");
+// console.log(lastName, "lastName");
+
+// // This is a number
+// var first = 1;
+// console.log(first, "first");
+
+// // This is also a string
+// var age = "24";
+// console.log(age, "age");
+
+// // This is a number
+// var numberAge = 24;
+// console.log(numberAge, "numberAge");
+
+// // This is a number
+// var decimalNumber = 2.5;
+// console.log(decimalNumber, "decimalNumber");
+
+// // This is a boolean
+// var knowsHtml = true;
+// var knowJS = false;
+// console.log(knowsHtml, "knowsHtml");
+// console.log(knowJS, "knowJS");
+
+// let stringExample = "Ivan";
+// let numberExample = 100;
+// let booleanExample = true;
+// let undefinedExample;
+// let nullExample = null;
+// console.log(typeof stringExample);
+// console.log(typeof numberExample);
+// console.log(typeof booleanExample);
+// console.log(typeof undefinedExample);
+// console.log(typeof nullExample);
+/*
+multiline, multiline, multiline, multiline, multiline
+multiline, multiline, multiline, multiline, multiline
+multiline, multiline, multiline, multiline, multiline
+multiline, multiline, multiline, multiline, multiline
+multiline, multiline, multiline, multiline, multiline
+multiline, multiline, multiline, multiline, multiline
+*/
+
+//arithmetic operators
+var a = 5;
+var b = 2;
+var c;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a++);
+console.log(a);
+console.log(++b);
+
+c = 5;
+console.log(c++);
+
+console.log(c + 9); // 15
+console.log(++c); // 7
+
+
+//comparison operators
+var x = 5;
+var y = 2;
+console.log(x == y); //false
+console.log(x != y); //true
+console.log(x > y); //true
+console.log(x < y); //false
+console.log(x >= y); //true
+console.log(x <= y); //false
+
+//asignment operators
+var x = 5;
+x += 2; // 7
+console.log(x);
+x -= 2; // 5
+console.log(x);
+x *= 2; // 10
+console.log(x);
+x /= 2; // 5
+console.log(x);
+x %= 2; // 1
+console.log(x);
+
